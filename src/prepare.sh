75587f4ba454238f3d4b2b140c4d42074cf67802
MOSGMI <marcolivier.sauer@gmi-aero.com>
2023-02-03 12:56:14 +0100

    test

--- src/prepare.sh
+++ src/prepare.sh
@@ -1,6 +1,6 @@
 jq -r .capif_hostname=\"$CAPIF_HOSTNAME\" /code/src/capif_NetApp_registration.json >> tmp.json && mv tmp.json /code/src/capif_NetApp_registration.json
-jq -r .capif_http_port=\"$CAPIF_PORT_HTTP\" /code/src/capif_NetApp_registration.json >> tmp.json && mv tmp.json /code/src/capif_NetApp_registration.json
-jq -r .capif_https_port=\"$CAPIF_PORT_HTTPS\" /code/src/capif_NetApp_registration.json >> tmp.json && mv tmp.json /code/src/capif_NetApp_registration.json
+jq -r .capif_port_http=\"$CAPIF_PORT_HTTP\" /code/src/capif_NetApp_registration.json >> tmp.json && mv tmp.json /code/src/capif_NetApp_registration.json
+jq -r .capif_port_https=\"$CAPIF_PORT_HTTPS\" /code/src/capif_NetApp_registration.json >> tmp.json && mv tmp.json /code/src/capif_NetApp_registration.json
 jq -r .capif_callback_url=\"http://$CAPIF_CALLBACK_URL\" /code/src/capif_NetApp_registration.json >> tmp.json && mv tmp.json /code/src/capif_NetApp_registration.json
 
 evolved5g register-and-onboard-to-capif --config_file_full_path="/code/src/capif_NetApp_registration.json"
